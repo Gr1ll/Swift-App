03dd3b58b310da7ef77ffce3af74cb73216702fe
Gr1ll <cyril.kurmann@gmail.com>
2024-06-27 11:19:26 +0200

    Initial Commit

---
+++ Test App/Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_AppApp.swift
+//  Test App
+//
+//  Created by Cyril Kurmann on 27.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
